1c8f43815cf8486ae0e888a8509c88d9e7481cf9
Thalita Alves de Freitas <thalitaalvesf@gmail.com>
2020-07-05 17:15:28 -0300

    tela de perguntas

--- app/src/main/java/com/example/quiz_app/features/quiz_questions/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quiz_app/features/quiz_questions/QuizQuestionsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import com.example.quiz_app.Constants
 import com.example.quiz_app.R
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,20 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private fun startQuestions(){
         val questionsList = Constants.getQuestion()
         Log.i("Questions size: ", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Questions? = questionsList[currentPosition - 1]
+
+        progress_bar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progress_bar.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.opOne
+        tv_option_two.text = question.opTwo
+        tv_option_three.text = question.opThree
+        tv_option_four.text = question.opFour
+
+
     }
 }
